d1b4090cac4710229e4392ab9b23f93e267f6d91
Mayank Madan <mayankmadan2602@gmail.com>
2025-11-10 17:12:30 +0530

    Implement BookService for book management

---
+++ backend/service/Book_Service.java
@@ -0,0 +1,33 @@
+package com.example.library.service;
+
+import com.example.library.model.Book;
+import com.example.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        Optional<Book> optional = bookRepository.findById(id);
+        return optional.orElse(null);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
